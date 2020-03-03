d414b96b706cfa377960d67454f29cbb49a7bc2b
Bangrawon <agungwar00@gmail.com>
2020-03-03 17:51:30 +0700

    hoho my first app done

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,57 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
     return MaterialApp(
       title: 'My First APP',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hello Word'),
         ),
-        body: Column(
+        body: ListView(
           children: <Widget>[
+            Image.asset(
+                'image/lake.jpg',
+                width: 600,
+                height: 240,
+                fit: BoxFit.cover,
+                ),
             titleSection,
+            Container(
+              child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildButtonColumn(color, Icons.call, 'CALL'),
+                  _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+                  _buildButtonColumn(color, Icons.share, 'SHARE'),
+                ],
+              ),
+            ),
+            textSection
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -53,6 +92,19 @@ class MyApp extends StatelessWidget {
         ),
         Text('41')
       ],
+    ),
+  );
+
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
     ),
   );
 }
